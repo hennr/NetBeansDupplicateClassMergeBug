5cfca6974ae9de9cab625214440b0a9b4b6a8d79
Jan-Hendrik Peters <j.peters@tarent.de>
2012-11-07 17:36:33 +0100

    add conflicts

--- src/main/java/name/hennr/netbeansdupplicateclassmergebug/Bar.java
+++ src/main/java/name/hennr/netbeansdupplicateclassmergebug/Bar.java
@@ -5,5 +5,5 @@ package name.hennr.netbeansdupplicateclassmergebug;
   * @author Jan-Hendrik Peters
  */
 public class Bar {
-	// master
+	// conflict
 }
--- src/main/java/name/hennr/netbeansdupplicateclassmergebug/Foo.java
+++ src/main/java/name/hennr/netbeansdupplicateclassmergebug/Foo.java
@@ -5,5 +5,5 @@ package name.hennr.netbeansdupplicateclassmergebug;
  * @author Jan-Hendrik Peters
  */
 public class Foo {
-	// master
+	// conflict
 }
